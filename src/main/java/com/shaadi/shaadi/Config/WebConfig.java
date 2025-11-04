8d0a7a3e0bb4926e86efbbd5f17efbcd344ac8ae
pankaj297 <pankajnaik297@gmail.com>
2025-11-04 21:11:17 +0530

    abc

--- src/main/java/com/shaadi/shaadi/Config/WebConfig.java
+++ src/main/java/com/shaadi/shaadi/Config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping(
+                "/**")
                 .allowedOrigins(
                         "http://localhost:5173",
                         "https://laganmelava.netlify.app")
